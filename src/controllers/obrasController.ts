d88e3689e7a1207c1a48b84150a1be4d35d70e19
Victor <victorfoliveira0407@gmail.com>
2025-12-12 14:10:31 -0300

    fix: arruma tratativa de erros no get all

--- src/controllers/obrasController.ts
+++ src/controllers/obrasController.ts
@@ -2,13 +2,29 @@ import * as obrasModel from "../models/obrasModel.js";
 import { Request, Response } from "express";
 
 export const getAllObras = async (req: Request, res: Response) => {
-    const obras = await obrasModel.findAllObras();
+    try {
+        const obras = await obrasModel.findAllObras();
+        const totalObras = obras.length;
 
-    res.status(200).json({
-        total: obras.length,
-        mensagem: obras.length === 0
-            ? "Nenhuma obra registrada no banco de dados"
-            : "As obras foram encontradas",
-        obras: obras
-    });
+        if (totalObras === 0) {
+            return res.status(404).json({
+                total: 0,
+                mensagem: "Nenhuma obra encontrada",
+                obras: []
+            });
+        }
+
+        return res.status(200).json({
+            total: totalObras,
+            mensagem: "Obras encontradas com sucesso",
+            obras
+        });
+    } catch (error) {
+        const detalhes = error instanceof Error ? error.message : "Erro desconhecido";
+
+        return res.status(500).json({
+            mensagem: "Erro interno do servidor",
+            detalhes
+        });
+    }
 }
